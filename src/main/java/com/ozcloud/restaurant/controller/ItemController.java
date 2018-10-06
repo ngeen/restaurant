ee737e2bdfa4ca3e66f4af1f14a26e627e1e2ea9
engin <o.enginoz@gmail.com>
2018-10-07 01:23:48 +0300

    düzenleme yapıldı. item ve productlar listelendi.

--- src/main/java/com/ozcloud/restaurant/controller/ItemController.java
+++ src/main/java/com/ozcloud/restaurant/controller/ItemController.java
@@ -2,10 +2,15 @@ package com.ozcloud.restaurant.controller;
 
 import com.google.common.collect.Lists;
 import com.ozcloud.restaurant.dtos.ItemDTO;
+import com.ozcloud.restaurant.dtos.ProductDTO;
 import com.ozcloud.restaurant.dtos.UserDTO;
+import com.ozcloud.restaurant.enums.ItemType;
 import com.ozcloud.restaurant.model.Item;
+import com.ozcloud.restaurant.model.Product;
 import com.ozcloud.restaurant.repository.ItemRepository;
+import com.ozcloud.restaurant.repository.ProductRepository;
 import com.ozcloud.restaurant.utils.BaseResponse;
+import com.sun.tools.javac.jvm.Items;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -30,14 +36,28 @@ public class ItemController implements Serializable {
     @Autowired
     private ItemRepository itemRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @GetMapping("/getItems")
     public ResponseEntity<BaseResponse> getAllItems() throws Exception {
         try {
             List<Item> items = Lists.newArrayList(itemRepository.findAll());
-            Type listType = new TypeToken<List<ItemDTO>>() {}.getType();
-            List<ItemDTO> itemList = modelMapper.map(items,listType);
+            List<Object> alltypes = new ArrayList<Object>();
+            for (Item item: items) {
+                Object oItem = null;
 
-            return ResponseEntity.ok(BaseResponse.getOkResponse(itemList));
+                if(item.getItemType() == ItemType.PRODUCT)
+                    oItem = productRepository.findById(item.getItemId()).orElse(null);
+                else
+                    oItem = item;
+
+                alltypes.add(oItem);
+            }
+            //Type listType = new TypeToken<List<ProductDTO>>() {}.getType();
+            //List<ProductDTO> itemList = modelMapper.map(items,listType);
+
+            return ResponseEntity.ok(BaseResponse.getOkResponse(alltypes));
         } catch (Exception e) {
             throw  new Exception(e);
         }

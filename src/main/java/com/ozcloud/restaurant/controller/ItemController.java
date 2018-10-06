cba1385fc3a066448e7e240acc6151f34cac6cdf
engin <o.enginoz@gmail.com>
2018-10-07 01:26:27 +0300

    java importlar organize edildi

--- src/main/java/com/ozcloud/restaurant/controller/ItemController.java
+++ src/main/java/com/ozcloud/restaurant/controller/ItemController.java
@@ -1,31 +1,20 @@
 package com.ozcloud.restaurant.controller;
 
 import com.google.common.collect.Lists;
-import com.ozcloud.restaurant.dtos.ItemDTO;
-import com.ozcloud.restaurant.dtos.ProductDTO;
-import com.ozcloud.restaurant.dtos.UserDTO;
 import com.ozcloud.restaurant.enums.ItemType;
 import com.ozcloud.restaurant.model.Item;
-import com.ozcloud.restaurant.model.Product;
 import com.ozcloud.restaurant.repository.ItemRepository;
 import com.ozcloud.restaurant.repository.ProductRepository;
 import com.ozcloud.restaurant.utils.BaseResponse;
-import com.sun.tools.javac.jvm.Items;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.Serializable;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 public class ItemController implements Serializable {
